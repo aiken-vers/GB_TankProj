4e959145eaf77c8fc0852bdd1584f79e7d754fdf
aiken-vers <dalexator@gmail.com>
2022-04-28 15:53:28 +0500

    Turret Base

---
+++ Source/TickTanks/Turret.cpp
@@ -0,0 +1,109 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Turret.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+// Sets default values
+ATurret::ATurret()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collision = CreateDefaultSubobject<UBoxComponent>("Collision");
+	RootComponent = Collision;
+
+	TurretBody = CreateDefaultSubobject<UStaticMeshComponent>("TurretBody");
+	TurretBody->SetupAttachment(RootComponent);
+	TurretHead = CreateDefaultSubobject<UStaticMeshComponent>("TurretHead");
+	TurretHead->SetupAttachment(TurretBody);
+	CannonSpawnPoint = CreateDefaultSubobject<UArrowComponent>("CannonSpawnPoint");
+	CannonSpawnPoint->SetupAttachment(TurretHead);
+	
+	TargetRange = CreateDefaultSubobject<USphereComponent>("TargetRange");
+	TargetRange->SetupAttachment(TargetRange);
+	TargetRange->OnComponentBeginOverlap.AddDynamic(this, &ATurret::OnTargetBeginOverlap);
+	TargetRange->OnComponentEndOverlap.AddDynamic(this, &ATurret::OnTargetEndOverlap);
+	
+
+}
+
+// Called when the game starts or when spawned
+void ATurret::BeginPlay()
+{
+	Super::BeginPlay();
+
+
+	auto Transform = CannonSpawnPoint->GetComponentTransform();
+	Cannon = GetWorld()->SpawnActor<ACannon>(CannonClass, Transform);	
+	Cannon->AttachToComponent(CannonSpawnPoint, FAttachmentTransformRules::SnapToTargetIncludingScale);
+	
+}
+
+void ATurret::Destroyed()
+{
+	Super::Destroyed();
+	
+	if(Cannon)
+		Cannon->Destroy();
+}
+
+// Called every frame
+void ATurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(BestTarget.IsValid())
+	{
+		auto Rotation = UKismetMathLibrary::FindLookAtRotation(TurretHead->GetComponentLocation(), BestTarget->GetActorLocation());
+		auto TurretRotation =  TurretHead->GetComponentRotation();		
+		//Rotation.Roll = 0;
+		//Rotation.Pitch = 0;
+		TurretHead->SetWorldRotation(FMath::Lerp(TurretRotation, Rotation, 0.1f));
+	}
+
+	
+
+}
+
+void ATurret::OnTargetBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if(Other==this)
+		return;
+	Targets.Add(Other);
+	if(!BestTarget.IsValid())
+	{
+		FindBestTarget();
+	}
+}
+
+void ATurret::OnTargetEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex)
+{
+	if(Other==this)
+		return;
+	Targets.Remove(Other);
+	if(Other == BestTarget)
+	{
+		FindBestTarget();
+	}
+}
+
+void ATurret::FindBestTarget()
+{
+	float MinDistance = 5000000;
+	BestTarget = nullptr;
+	for(auto Target : Targets)
+	{
+		auto Distance = FVector::Dist2D(GetActorLocation(), Target->GetActorLocation());
+		if(MinDistance > Distance)
+		{
+			MinDistance = Distance;
+			BestTarget = Target;
+		}
+	}
+}
+
---
+++ Source/TickTanks/Turret.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Cannon.h"
+#include "GameFramework/Actor.h"
+#include "Turret.generated.h"
+
+
+class UArrowComponent;
+class UStaticMeshComponent;
+class UBoxComponent;
+
+UCLASS()
+class TICKTANKS_API ATurret : public AActor
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UBoxComponent* Collision;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	USphereComponent* TargetRange;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* TurretBody;
+	
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* TurretHead;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UArrowComponent* CannonSpawnPoint;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Armory")
+	TSubclassOf<ACannon> CannonClass;
+	UPROPERTY(VisibleInstanceOnly, BlueprintReadOnly, Category="Armory")
+	ACannon* Cannon;
+	
+	// Sets default values for this actor's properties
+	ATurret();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void Destroyed() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+
+	UFUNCTION()
+	void OnTargetBeginOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	UFUNCTION()
+	void OnTargetEndOverlap(UPrimitiveComponent* OverlappedComp, AActor* Other, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	void FindBestTarget();
+
+	TWeakObjectPtr<AActor> BestTarget;
+
+	UPROPERTY()
+	TArray<AActor*> Targets;
+};
